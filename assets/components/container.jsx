097e82a0a7a15d2e9317eb01d02a83778e37d387
Nikolai Basik <Nikolai_Basik@epam.com>
2016-11-18 00:47:53 +0300

    started with header

--- assets/components/container.jsx
+++ assets/components/container.jsx
@@ -1,13 +1,47 @@
 'use strict';
 
 import React from 'react';
+import {FormGroup, FormControl, Checkbox} from 'react-bootstrap';
 
 export default class Container extends React.Component {
-    render(){
+    render() {
         return (
-            <div>
-                <h1>Hello!</h1>
+            <div className="app-container">
+                <Header/>
+                <Body />
             </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="container-header">
+                <span className="h3">ToDo List</span>
+                <Checkbox className="pull-right">Show active</Checkbox>
+                <Search />
+            </div>
+        );
+    }
+}
+
+class Body extends React.Component {
+    render() {
+        return (
+            <div className="container-body">
+                <p>Body</p>
+            </div>
+        );
+    }
+}
+
+class Search extends React.Component {
+    render() {
+        return (
+            <FormGroup className="pull-right">
+                <FormControl type="text" placeholder="Search"/>
+            </FormGroup>
         );
     }
 }
